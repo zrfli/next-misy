73b7669f8470f1d653d0b4f86fb9f037c9db6d7f
Eray <78818724+zrfli@users.noreply.github.com>
2025-01-09 12:16:49 +0300

    Update index.tsx

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import LogoPath from "@/public/logo.png";
+import LogoPath from "@/public/logo.svg";
 
 interface LogoProps { LogoClass?: string }
 
